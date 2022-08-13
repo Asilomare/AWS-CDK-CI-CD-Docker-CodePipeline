dad57a43f307c3a275a8a3f42a593880d616e8f9
Asilomare <104533101+Asilomare@users.noreply.github.com>
2022-08-13 09:45:25 -0400

    Update scraper.py

--- scraper.py/scraper.py
+++ scraper.py/scraper.py
@@ -1,26 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import boto3
+import json
 
-URL = "https://www.worldometers.info/coronavirus/"
-page = requests.get(URL)
-
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-
-results = soup.findAll("div", class_='maincounter-number')
-
-data_dict = {"Cases":0, "Deaths":0, "Recovered":0}
-c = 0
-for span in results:
-    data_dict[c]=span.text
-    c = c+1
+s3 = boto3.client('s3')
+def lambda_handler(event, context):
+    URL = "https://www.worldometers.info/coronavirus/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.findAll("div", class_='maincounter-number')
     
+    bucket = 'webscraperbucket123123'
     
-data_list = []
-for span in results:
-    data_list.append(span.text)
-    print(span.text)
+    transcationToUpload = {}
+    transcationToUpload['cases'] = results[0].text
+    transcationToUpload['deaths'] = results[1].text
+    transcationToUpload['recovered'] = results[2].text
     
-
-
+    file_name = 'data' + '.json'
+    uploadByteStream = bytes(json.dumps(transcationToUpload).encode('UTF-8'))
+    
+    s3.put_object(Bucket=bucket, Key=file_name, Body=uploadByteStream)
