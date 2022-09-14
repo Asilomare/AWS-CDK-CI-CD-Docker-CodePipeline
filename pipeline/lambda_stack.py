dd329d20473e143b0346482d3458de44f73e9f14
Asilomare <104533101+Asilomare@users.noreply.github.com>
2022-09-14 17:20:12 -0400

    Add files via upload

---
+++ pipeline/lambda_stack.py
@@ -0,0 +1,32 @@
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_events as events,
+    aws_iam as iam,
+    aws_s3 as s3,
+    aws_events_targets as targets,
+    App, Stack, Duration
+)
+#make portable s3 bucket
+#put_object permissions
+
+# https://docs.aws.amazon.com/cdk/api/v1/python/modules.html
+class LambdaStack(Stack):
+    def __init__(self, app: App, id: str) -> None:
+        super().__init__(app, id)
+
+        #inlined lambda function (see lambda_function.py)
+        function = _lambda.Function(self, "lambda_function",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="lambda_function.handler",
+            code=_lambda.Code.from_asset("lambda"),
+            timeout=Duration.seconds(150)
+        )
+    
+        #eventbridge daily trigger
+        rule = events.Rule(self, "Run Daily",
+            schedule=events.Schedule.cron(minute="0", hour="5", week_day="*", month="*", year="*")
+        )
+        rule.add_target(targets.LambdaFunction(function))
+        
+        bucket = s3.Bucket.from_bucket_name(self, "existingBucket", "webscraperbucket123123")
+        bucket.grant_put(function)
---
+++ pipeline/pipeline_stack.py
@@ -0,0 +1,26 @@
+import aws_cdk as cdk
+from constructs import Construct
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+
+from pipeline.lambda_stack import LambdaStack
+
+class LambdaStage(cdk.Stage):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        LambdaStack(self, "LambdaStack")
+    
+class PipelineStack(cdk.Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        pipeline =  CodePipeline(self, "Pipeline", 
+                        pipeline_name="Pipeline",
+                        synth=ShellStep("Synth", 
+                            input=CodePipelineSource.git_hub("Asilomare/WebScraper", "master"),
+                            commands=["python -m pip install -r lambda-requirements.txt -t lambda",
+                                "python -m pip install -r requirements.txt",
+                                "npx cdk synth"]
+                        )
+                    )
