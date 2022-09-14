925859d8ccbb6dece0bad58daf57893bab77f424
Asilomare <104533101+Asilomare@users.noreply.github.com>
2022-09-14 18:06:43 -0400

    Update pipeline_stack.py

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -7,20 +7,23 @@ from pipeline.lambda_stack import LambdaStack
 class LambdaStage(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        
         LambdaStack(self, "LambdaStack")
+    
     
 class PipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        source = CodePipelineSource.git_hub("Asilomare/WebScraper", "master")
         pipeline =  CodePipeline(self, "Pipeline", 
                         pipeline_name="Pipeline",
                         synth=ShellStep("Synth", 
-                            input=CodePipelineSource.git_hub("Asilomare/WebScraper", "master"),
+                            input=source,
                             commands=["python -m pip install -r lambda-requirements.txt -t lambda",
                                 "python -m pip install -r requirements.txt",
                                 "npx cdk synth"]
                         )
                     )
+        pipeline.add_stage(LambdaStage(self, 'lambdastage'))
