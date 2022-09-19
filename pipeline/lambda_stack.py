8c4b661ec4135cdb60202d63ef2c1b6be49d2870
Asilomare <104533101+Asilomare@users.noreply.github.com>
2022-09-19 09:25:48 -0400

    Update lambda_stack.py

--- pipeline/lambda_stack.py
+++ pipeline/lambda_stack.py
@@ -6,27 +6,30 @@ from aws_cdk import (
     aws_events_targets as targets,
     App, Stack, Duration
 )
-#make portable s3 bucket
-#put_object permissions
 
-# https://docs.aws.amazon.com/cdk/api/v1/python/modules.html
 class LambdaStack(Stack):
     def __init__(self, app: App, id: str) -> None:
         super().__init__(app, id)
 
-        #inlined lambda function (see lambda_function.py)
-        function = _lambda.Function(self, "lambda_function",
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            handler="lambda_function.handler",
-            code=_lambda.Code.from_asset("lambda"),
-            timeout=Duration.seconds(150)
+        bucket = s3.Bucket(self, "WebScraperDumpBucket")
+        
+        
+        #lambda from docker image
+        function = _lambda.DockerImageFunction(self, "lambda_function_docker",
+            code=_lambda.DockerImageCode.from_image_asset(
+                directory="lambda/"),
+            timeout=Duration.seconds(150),
+            environment={ 
+                 "bucket": bucket.bucket_name,
+            }
         )
     
+        bucket.grant_put(function)
+        
         #eventbridge daily trigger
         rule = events.Rule(self, "Run Daily",
             schedule=events.Schedule.cron(minute="0", hour="5", week_day="*", month="*", year="*")
         )
         rule.add_target(targets.LambdaFunction(function))
         
-        bucket = s3.Bucket.from_bucket_name(self, "existingBucket", "webscraperbucket123123")
-        bucket.grant_put(function)
+        
