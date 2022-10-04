5727e3716a0abdee0251c2eb79562f7a92e96a43
Asilomare <104533101+Asilomare@users.noreply.github.com>
2022-10-04 12:26:32 -0400

    Update pipeline_stack.py

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -22,7 +22,7 @@ class PipelineStack(cdk.Stack):
                         synth=ShellStep("Synth", 
                             input=source,
                             commands=[
-                                "npm install pytest",
+                                "python -m pip install -r requirements-dev.txt",
                                 "python -m pip install -r requirements.txt",
                                 "npx cdk synth"
                                 ]
