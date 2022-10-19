9725cedfd76b1f1cb5e002104be3ddee87092369
Asilomare <104533101+Asilomare@users.noreply.github.com>
2022-10-19 15:51:57 -0400

    Update pipeline_stack.py

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -1,16 +1,16 @@
 import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
-
+import aws_cdk.aws_codepipeline_actions as codepipeline_actions
 from pipeline.lambda_stack import LambdaStack
+from aws_cdk.aws_lambda import Function
 
 class LambdaStage(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
         LambdaStack(self, "LambdaStack")
-    
-    
+
 class PipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -23,10 +23,21 @@ class PipelineStack(cdk.Stack):
                             input=source,
                             commands=[
                                 "python -m pip install -r requirements.txt",
-                                "npx pytest unittests",
                                 "npx cdk synth"
                                 ]
                         )
                     )
+        
+        #lambda test 
+        function = Function.from_function_name(self,
+            "lambda_function_docker", "lambda_function_docker"
+            )
+        codepipeline_actions.LambdaInvokeAction(
+            action_name = "invoke_Lambda_Action",
+            lambda_ = function
+            )
+            
+        #lambda stage           
         pipeline.add_stage(LambdaStage(self, 'lambdastage'))
+        #invoke lambda stage
 
